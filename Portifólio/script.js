176e1d792af07431170a747f048b457fa0dfcc46
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-04-16 16:10:57 -0300

    add about and style

--- Portifólio/script.js
+++ Portifólio/script.js
@@ -11,7 +11,8 @@ const home = '<div class="homeHeader-container">'
     +'<img src="./imagens/logoRafaExpressWhite.png" alt="logo-rafaexpress" width="250px"/>'
   +'</a>'
 +'</div>'
-+'<h3>Projetos na Trybe:</h3>';
++'<h3>Projetos na Trybe:</h3><br>'
++'<a class="link-home-github" href="https://github.com/RafaelBiatoAzevedo" target="blank"><img src="./imagens/logo_github.png" alt="logo-gihub" width="70px"/></a>';
 
 const formation = '<h2>Formação</h2>'
 +'<p class="text-principal"><strong>Trybe</strong><br>Desenvolvimento Web Full Stack 1° Semestre<br>Situação: Cursando</p>'
@@ -56,6 +57,15 @@ const courses = '<h2>Cursos</h2>'
 const experiences = '<h2>Experiências Profissionais</h2>'
 +'<p class="text-principal"><strong>Restaurante Bandeirantes</strong><br>Av. Bandeirantes 1640, Mogi-Guaçu - SP<br>Cargo: Gerente<br>De: Janeiro/2003 - Até: Maio/2018</p>'
  
+
+const about = '<h2>Sobre mim</h2>'
++'<p class="text-principal">Iniciei estudos na área de TI desde muito jovem.<br>'
++'Em 1995, com 9 anos iniciei os estudos em informática,<br>'
++'Aos 15 anos fiz curso de manutenção e instalação de redes e computadores,<br>'
++'Aos 19 estudei 4 anos de Análise de Sistema na Puc-Campinas,<br>'
++'Trabalhei 15 anos profissionalmente como Gerente no ramo alimentício,<br>'
++'Em 2020 decidi mudar e trabalhar com TI que realmente amo e retomei os estudos em desenvolvimento web full stack na Trybe.</p>';
+
 const contacts = '<h2>Contato</h2>'
 +'<p class="text-principal">'
   +'<strong>Endereço:</strong> Rua Cleonice Ap. Cruz Thielle, 350 - Jd. Alvorada<br>Cep: 13841-077, Mogi-Guaçu - SP'
@@ -65,7 +75,7 @@ const contacts = '<h2>Contato</h2>'
 +'<div class="links-container">'
   +'<a class="link" href="https://www.linkedin.com/in/rafael-azevedo-530889195/" target="blank"><img src="./imagens/logo_linke.png" alt="logo-linke" width="50px"/></a>'
   +'<a class="link" href="https://github.com/RafaelBiatoAzevedo" target="blank"><img src="./imagens/logo_github.png" alt="logo-gihub" width="60px"/></a>'
-  +'<a href="https://www.instagram.com/rafael_azevedo_55/" target="blank"><img src="./imagens/logo_insta.png" alt="logo-insta" width="50px"/></a>'
+  +'<a class="link" href="https://www.instagram.com/rafael_azevedo_55/" target="blank"><img src="./imagens/logo_insta.png" alt="logo-insta" width="50px"/></a>'
 +'</div>';
 
 
@@ -87,29 +97,39 @@ const showInformations = (evt) => {
       mainContainer.innerHTML = experiences;
       break;
     case 'spanAbout':
+      mainContainer.innerHTML = about;
       break;
     case 'spanContact':
       mainContainer.innerHTML = contacts;
       break;
     default:
+      mainContainer.innerHTML = home;
   }
 };
 
+const showHome = () => {
+  const mainContainer = document.querySelector('#centerContent');
+  mainContainer.innerHTML = '';
+  mainContainer.innerHTML = home;
+
+}
+
 const createEvents = () => {
-  const home = document.querySelector('#spanHome');
-  home.addEventListener('click', showInformations);
-  const formation = document.querySelector('#spanFormation');
-  formation.addEventListener('click', showInformations)
-  const courses = document.querySelector('#spanCourses');
-  courses.addEventListener('click', showInformations)
-  const experiences = document.querySelector('#spanExperiences');
-  experiences.addEventListener('click', showInformations)
-  const about = document.querySelector('#spanAbout');
-  about.addEventListener('click', showInformations)
-  const contact = document.querySelector('#spanContact');
-  contact.addEventListener('click', showInformations)
+  const home = document
+    .querySelector('#spanHome').addEventListener('click', showInformations);
+  const formation = document
+    .querySelector('#spanFormation').addEventListener('click', showInformations);
+  const courses = document
+    .querySelector('#spanCourses').addEventListener('click', showInformations);
+  const experiences = document
+    .querySelector('#spanExperiences').addEventListener('click', showInformations);
+  const about = document
+    .querySelector('#spanAbout').addEventListener('click', showInformations);
+  const contact = document
+    .querySelector('#spanContact').addEventListener('click', showInformations);
 };
 
 window.onload = () => {
   createEvents();
+  showHome();
 };
