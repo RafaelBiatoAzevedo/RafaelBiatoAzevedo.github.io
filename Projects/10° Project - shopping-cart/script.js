8fbbe8b67453740d48530009df4a918e4fe2b2ed
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-05-07 01:38:16 -0300

    add projects zoo-function and shopping-cart e css responsive

---
+++ Projects/10° Project - shopping-cart/script.js
@@ -0,0 +1,154 @@
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+function setCounter() {
+  const listItems = document.querySelectorAll('.cart__item');
+  document.querySelector('.span-counter').innerHTML = listItems.length;
+}
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function startLoading() {
+  const loading = document.querySelector('.loading');
+  loading.innerText = 'loading...';
+}
+
+function stopLoading() {
+  const loading = document.querySelector('.loading');
+  loading.remove();
+}
+
+function sumItems() {
+  return new Promise((resolve, reject) => {
+    const myCart = document.querySelectorAll('.cart__item');
+    let price = '';
+    let sum = 0;
+    myCart.forEach((value) => {
+      const position = value.innerText.indexOf('$');
+      for (let index = position + 1; index < value.innerText.length; index += 1) {
+        price += value.innerText[index];
+      }
+      sum += Number(price);
+      price = '';
+    });
+    if (sum >= 0) {
+      resolve(sum);
+    } else {
+      reject('Erro! Soma inválida');
+    }
+  });
+}
+
+async function putPrice() {
+  let price;
+  try {
+    price = await sumItems();
+    const elementPrice = document.querySelector('.total-price');
+    if (price === 0) elementPrice.innerText = '0,00';
+    else elementPrice.innerText = `${price.toFixed(2)}`;
+  } catch (error) {
+    window.alert(error);
+  }
+}
+
+function saveMyCart() {
+  const listItems = document.querySelector('.cart__items');
+  localStorage.setItem('myCart', listItems.innerHTML);
+}
+
+function cartItemClickListener(event) {
+  const list = document.querySelector('.cart__items');
+  list.removeChild(event.target);
+  putPrice();
+  saveMyCart();
+  setCounter();
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function emptyCart() {
+  const list = document.querySelector('.cart__items');
+  list.innerHTML = '';
+  putPrice();
+  saveMyCart();
+  setCounter();
+}
+
+function restoreMyCart() {
+  document.querySelector('.cart__items').innerHTML = localStorage.getItem('myCart');
+  const items = document.querySelectorAll('.cart__item');
+  if (items !== null) {
+    items.forEach(value => value.addEventListener('click', cartItemClickListener));
+  }
+  putPrice();
+}
+
+function addEventItemInMyCart(element, id) {
+  element.lastChild.addEventListener('click', () => {
+    fetch(`https://api.mercadolibre.com/items/${id}`)
+      .then(resp => resp.json()
+      .then((item) => {
+        const obj = { sku: item.id, name: item.title, salePrice: item.price };
+        const li = createCartItemElement(obj);
+        const listItems = document.querySelector('.cart__items');
+        listItems.appendChild(li);
+        putPrice();
+        saveMyCart();
+        setCounter();
+      }));
+  });
+}
+
+function loadingItems() {
+  startLoading();
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+    .then((response) => {
+      response.json()
+        .then(item => item.results.forEach((value) => {
+          const obj = { sku: value.id, name: value.title, image: value.thumbnail };
+          const section = createProductItemElement(obj);
+          addEventItemInMyCart(section, value.id);
+          const containerItems = document.querySelector('.items');
+          containerItems.appendChild(section);
+        }));
+      stopLoading();
+    });
+}
+
+window.onload = function onload() {
+  restoreMyCart();
+  loadingItems();
+  setCounter();
+  document.querySelector('.empty-cart').addEventListener('click', emptyCart);
+};
---
+++ Projects/9° Project - zoo-functions/src/zoo.js
@@ -0,0 +1,243 @@
+/*
+eslint no-unused-vars: [
+  "error",
+  {
+    "args": "none",
+    "vars": "local",
+    "varsIgnorePattern": "data"
+  }
+]
+*/
+
+const data = require('./data');
+
+function animalsByIds(...ids) {
+  // seu código aqui
+  return ids.reduce((species, currentValue) => {
+    species.push(data.animals.find(animal => animal.id === currentValue));
+    return species;
+  }, []);
+}
+
+function animalsOlderThan(animal, age) {
+  // seu código aqui
+  let result;
+  data.animals.forEach((specie) => {
+    if (specie.name === animal) {
+      result = specie.residents.every(resident => resident.age >= age);
+    }
+  });
+  return result;
+}
+
+function employeeByName(employeeName) {
+  // seu código aqui
+  let result;
+  if (employeeName === undefined) {
+    result = {};
+  } else {
+    result = data.employees.find(
+      employee => employee.firstName === employeeName || employee.lastName === employeeName);
+  }
+  return result;
+}
+
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+  const employee = {
+    id: personalInfo.id,
+    firstName: personalInfo.firstName,
+    lastName: personalInfo.lastName,
+    managers: associatedWith.managers,
+    responsibleFor: associatedWith.responsibleFor,
+  };
+  return employee;
+}
+
+function isManager(id) {
+  // seu código aqui
+  let resultTemp;
+  let resultManager = false;
+  data.employees.forEach((employee) => {
+    resultTemp = employee.managers.some(manager => manager === id);
+    if (resultTemp === true) {
+      resultManager = true;
+    }
+  });
+  return resultManager;
+}
+
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  // seu código aqui
+  data.employees.push(createEmployee({ id, firstName, lastName }, { managers, responsibleFor }));
+}
+
+function animalCount(species) {
+  // seu código aqui
+  let result;
+  if (species === undefined) {
+    result = data.animals.reduce((objSpecies, currentSpecie) => {
+      objSpecies[currentSpecie.name] = currentSpecie.residents.length;
+      return objSpecies;
+    }, {});
+  } else {
+    data.animals.forEach((specie) => {
+      if (species === specie.name) {
+        result = specie.residents.length;
+      }
+    });
+  }
+  return result;
+}
+
+function entryCalculator(entrants) {
+  // seu código
+  let result;
+  const objPrices = data.prices;
+  if (entrants === undefined || entrants === {}) {
+    result = 0;
+  } else {
+    const { Child = 0, Adult = 0, Senior = 0 } = entrants;
+    result = (Child * objPrices.Child) + (Adult * objPrices.Adult) + (Senior * objPrices.Senior);
+  }
+  return result;
+}
+
+function createObj() {
+  const result = data.animals.reduce((objResult, currentValue) => {
+    objResult[`${currentValue.location}`] = [];
+    return objResult;
+  }, {});
+  return result;
+}
+
+function regionAndSpecies() {
+  const result = createObj();
+  data.animals.map(value => result[`${value.location}`].push(value.name));
+  return result;
+}
+
+function includeName(sort = false) {
+  const result = createObj();
+  data.animals.forEach((value) => {
+    const objNames = { [`${value.name}`]: [] };
+    value.residents.forEach((animal) => {
+      objNames[`${value.name}`].push(`${animal.name}`);
+    });
+    if (sort === true) {
+      objNames[`${value.name}`].sort();
+    }
+    result[`${value.location}`].push(objNames);
+  });
+  return result;
+}
+
+function sexNames(sex, sort = false) {
+  const result = createObj();
+  data.animals.forEach((value) => {
+    const objNames = { [`${value.name}`]: [] };
+    value.residents.forEach((animal) => {
+      if (animal.sex === sex) {
+        objNames[`${value.name}`].push(`${animal.name}`);
+      }
+    });
+    if (sort === true) {
+      objNames[`${value.name}`].sort();
+    }
+    result[`${value.location}`].push(objNames);
+  });
+  return result;
+}
+
+function animalMap(options) {
+  // seu código aqui
+  if (options === undefined || options.includeNames !== true) return regionAndSpecies();
+  if (options.sex !== undefined) return sexNames(options.sex, options.sorted);
+  if (options.sorted) return includeName(options.sorted);
+  return includeName();
+}
+
+function schedule(dayName) {
+  // seu código aqui
+  const objHours = data.hours;
+  const keys = Object.keys(objHours);
+  let result = keys.reduce((objResult, currentValue) => {
+    if (objHours[currentValue].open === 0 && objHours[currentValue].close === 0) {
+      objResult[currentValue] = 'CLOSED';
+    } else {
+      objResult[currentValue] = `Open from ${objHours[currentValue].open}am until ${objHours[currentValue].close - 12}pm`;
+    }
+    return objResult;
+  }, {});
+  if (dayName !== undefined) {
+    keys.forEach((key) => {
+      if (key === dayName) {
+        result = { [key]: result[key] };
+      }
+    });
+  }
+  return result;
+}
+
+function oldestFromFirstSpecies(id) {
+  // seu código aqui
+  const employee = data.employees.find(value => value.id === id);
+  const specie = data.animals.find(value => value.id === employee.responsibleFor[0]);
+  const animalResult = specie.residents.reduce((oldAnimal, currentValue) => {
+    let result;
+    if (currentValue.age >= oldAnimal.age) {
+      result = currentValue;
+    } else {
+      result = oldAnimal;
+    }
+    return result;
+  });
+  return Object.values(animalResult);
+}
+
+function increasePrices(percentage) {
+  // seu código aqui
+  Object.keys(data.prices).forEach((key) => {
+    const price = (data.prices[key] + (data.prices[key] * (percentage / 100)) + 0.001);
+    data.prices[key] = Number(price.toFixed(2));
+  });
+}
+
+function employeeCoverage(idOrName) {
+  // seu código aqui
+  let result = data.employees.reduce((objResult, currentEmployee) => {
+    objResult[`${currentEmployee.firstName} ${currentEmployee.lastName}`] = [];
+    currentEmployee.responsibleFor.forEach((idSpecie) => {
+      data.animals.forEach((animal) => {
+        if (animal.id === idSpecie) {
+          objResult[`${currentEmployee.firstName} ${currentEmployee.lastName}`].push(animal.name);
+        }
+      });
+    });
+    return objResult;
+  }, {});
+  if (idOrName !== undefined) {
+    const employee = data.employees.find(value => value.firstName === idOrName ||
+      value.lastName === idOrName || value.id === idOrName);
+    result = {
+      [`${employee.firstName} ${employee.lastName}`]: result[`${employee.firstName} ${employee.lastName}`],
+    };
+  }
+  return result;
+}
+
+module.exports = {
+  entryCalculator,
+  schedule,
+  animalCount,
+  animalMap,
+  animalsByIds,
+  employeeByName,
+  employeeCoverage,
+  addEmployee,
+  isManager,
+  animalsOlderThan,
+  oldestFromFirstSpecies,
+  increasePrices,
+  createEmployee,
+};
